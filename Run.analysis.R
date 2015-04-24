3dd532eb9990e0586abf92827c47dc424fdf5719
BIA <biglesia@users.noreply.github.com>
2015-04-24 14:38:38 +0200

    Create Run.analysis.R

---
+++ Run.analysis.R
@@ -0,0 +1,56 @@
+# Pasos del proyecto:
+
+# 1. Fusión de datos para crear la fuente de datos.
+
+tmp1 <- read.table("train/X_train.txt")
+tmp2 <- read.table("test/X_test.txt")
+X <- rbind(tmp1, tmp2)
+
+tmp1 <- read.table("train/subject_train.txt")
+tmp2 <- read.table("test/subject_test.txt")
+S <- rbind(tmp1, tmp2)
+
+tmp1 <- read.table("train/y_train.txt")
+tmp2 <- read.table("test/y_test.txt")
+Y <- rbind(tmp1, tmp2)
+
+# 2. Extracción de media y desviación típica para cada medición.
+
+features <- read.table("features.txt")
+indices_of_good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
+X <- X[, indices_of_good_features]
+names(X) <- features[indices_of_good_features, 2]
+names(X) <- gsub("\\(|\\)", "", names(X))
+names(X) <- tolower(names(X))
+
+# 3. Utilizar nombres descriptivos  para nombrar las actividades.
+
+activities <- read.table("activity_labels.txt")
+activities[, 2] = gsub("_", "", tolower(as.character(activities[, 2])))
+Y[,1] = activities[Y[,1], 2]
+names(Y) <- "activity"
+
+# 4. Etiquetar adecuadamente el conjunto de datos con nombres descriptivos.
+
+names(S) <- "subject"
+cleaned <- cbind(S, Y, X)
+write.table(cleaned, "fusión_datos_limpios.txt")
+
+# 5. Crear un segundo conjunto de datos con el promedio de cada variable para cada actividad y materia.
+uniqueSubjects = unique(S)[,1]
+numSubjects = length(unique(S)[,1])
+numActivities = length(activities[,1])
+numCols = dim(cleaned)[2]
+result = cleaned[1:(numSubjects*numActivities), ]
+
+row = 1
+for (s in 1:numSubjects) {
+  for (a in 1:numActivities) {
+    result[row, 1] = uniqueSubjects[s]
+    result[row, 2] = activities[a, 2]
+    tmp <- cleaned[cleaned$subject==s & cleaned$activity==activities[a, 2], ]
+    result[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
+    row = row+1
+  }
+}
+write.table(result, "Datos_proyecto_coursera.txt")
